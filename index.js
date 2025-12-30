6afb6eadd37403ab28027a52d9d1ac1a263d0c2d
Ryodan0001 <tugaybyr52@gmail.com>
2025-12-30 21:13:58 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer')
+const http = require('http')
+
+// Render'ın uyumaması için gereken web sunucusu
+http.createServer((req, res) => {
+    res.write("Bot Aktif ve Sunucuda!");
+    res.end();
+}).listen(8080);
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'RYODANN_IP_BURAYA', // Buraya FalixNodes IP'ni yaz
+        port: 25565,                // Portun farklıysa düzelt
+        username: 'Bot_724', 
+        version: '1.21.1'
+    })
+
+    bot.on('spawn', () => console.log('Bot sunucuya girdi!'))
+    bot.on('end', () => {
+        console.log('Bağlantı koptu, 10 saniye sonra tekrar denenecek...')
+        setTimeout(createBot, 10000)
+    })
+    bot.on('error', err => console.log('Hata oluştu:', err))
+}
+
+createBot()
